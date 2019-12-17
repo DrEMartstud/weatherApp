9176b6b2311d08815f10033f822542141d92ac32
DrEMartstud <dremartstud@gmail.com>
2019-12-17 18:17:53 +0200

    WeatherApp
    загрузил код на гит

---
+++ WeatherApp/view/SearchMenuViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SearchMenuViewController.swift
+//  WeatherApp
+//
+//  Created by Андрей Головченко on 15.12.2019.
+//  Copyright © 2019 DrEMartstud. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherApp/view/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Андрей Головченко on 14.12.2019.
+//  Copyright © 2019 DrEMartstud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+let apiKey = "35f73bd26a806e34af48af53aeaedeff"
+let cityName = "Kaliningrad"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    let urlString = "http://api.weatherstack.com/current?access_key=35f73bd26a806e34af48af53aeaedeff&query=Kaliningrad"
+    //let urlString = "http://api.weatherstack.com/current?access_key=\(apiKey)&query=\(cityName)"
+          let url = URL(string: urlString)
+          let session = URLSession.shared
+    session.dataTask(with: url) { (data, response, error) in
+    if let response = response {
+        print(response)
+        }
+    guard let data = data else {return}
+    print(data)
+    
+    do {
+    let json = try JSONSerialization.jsonObject(with: data, options: [])
+    
+    }
+    }
+    
+    session.resume()
+
+}
